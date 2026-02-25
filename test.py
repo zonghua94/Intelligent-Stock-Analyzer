a6c7e2e8ccd16d8bdd6fc2bbfac8ec74463df7b2
yuhuan.zh <yuhuan.zh@alibaba-inc.com>
2026-02-25 11:51:44 +0800

    update

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from framework.stock_filter import StockFilter
+from utils.logger import Logger
+
+logger = Logger(__name__)
 
 sf = StockFilter()
 filtered_stock_codes = sf.filter_stocks()
+logger.info(filtered_stock_codes)
